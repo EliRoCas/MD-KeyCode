aa777bddb1f34b2286b1ecc3908562b2dcfab5af
Eli RoCas <elir_94@hotmail.com>
2024-09-14 08:29:48 -0500

    add: nav component

---
+++ exercises/04-week/src/components/layout/Nav.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+const Nav = () => {
+  return (
+    <div className="tab">
+      <button>Agregar Nueva tarea </button>
+      <button>Ver las Tareas</button>
+    </div>
+  );
+};
+
+Nav.propTypes = {};
+
+export default Nav;
